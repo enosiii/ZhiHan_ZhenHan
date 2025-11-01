b7766047f5c802d22f0ea010c915e4b114dff483
enosiii <enosrigortan@gmail.com>
2025-11-01 23:18:20 +0800

    add app

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'ZhiHan_ZhenHan-v1';
+const ASSETS_TO_CACHE = [
+  '/ZhiHan_ZhenHan/',
+  '/ZhiHan_ZhenHan/index.html',
+  '/ZhiHan_ZhenHan/images/zh192.png',
+  '/ZhiHan_ZhenHan/images/zh512.png'
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Serve cached assets when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
